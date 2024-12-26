07f121d733e9c753d2ddc9f31e4c98f9f2928da7
ahmedmlaouhia <ahmedmlaouhia6@gmail.com>
2024-12-26 02:12:43 +0100

    mkbhi

--- dist/dto/user.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserResponse = void 0;
-/**
- * Represents the response data for a user.
- */
-class UserResponse {
-}
-exports.UserResponse = UserResponse;
